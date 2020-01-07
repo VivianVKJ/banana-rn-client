4f90450330b668e76b2f1cb6d9b151e3e6e17510
VivianVKJ <1067937107@qq.com>
2020-01-06 21:08:15 -0500

    registration page upload img

--- src/screens/RegistrationScreen/RegistrationScreen.styles.ts
+++ src/screens/RegistrationScreen/RegistrationScreen.styles.ts
@@ -2,20 +2,25 @@ import { StyleSheet, Dimensions } from 'react-native';
 import * as colors from '../../util/colors';
 
 export default StyleSheet.create({
-  outerContainer: {
-    flex: 1,
-    backgroundColor: colors.BANANA_YELLOW,
+	outerContainer: {
+		flex: 1,
+		backgroundColor: colors.BANANA_YELLOW,
 		paddingHorizontal: '10%',
 		justifyContent: 'space-between',
-  },
-  input: {
-      height: 28,
-      marginBottom: 14,
-      paddingLeft: 10,
-  },
-  text: {
-      fontFamily: 'open-sans-light',
-      fontSize: 14,
-      color: colors.NAVY_BLUE,
-  },
+	},
+	input: {
+		height: 28,
+		marginBottom: 14,
+		paddingLeft: 10,
+	},
+	text: {
+		fontFamily: 'open-sans-light',
+		fontSize: 14,
+		color: colors.NAVY_BLUE,
+	},
+	checkboxRow: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
